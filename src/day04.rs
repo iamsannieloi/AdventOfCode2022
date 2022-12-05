73df679d76581602bce848116d91bcc136afec59
iamsannieloi <iamsannieloi@gmail.com>
2022-12-05 01:55:45 -0500

    fix small issue

--- src/day04.rs
+++ src/day04.rs
@@ -14,10 +14,12 @@ pub fn solution() -> i32 {
 fn is_contain_section(sections_info: &String) -> bool {
   // println!("STRING: {:?}", sections_info);
   let split : Vec<&str> = sections_info.split(",").collect();
-  let elf_one: Vec<&str> = split[0].split("-").collect();
-  let elf_two: Vec<&str> = split[1].split("-").collect();
+  let elf_one: Vec<i32> = split[0].split("-").collect::<Vec<&str>>().iter().map(|x| x.parse().unwrap()).collect();
+  let elf_two: Vec<i32> = split[1].split("-").collect::<Vec<&str>>().iter().map(|x| x.parse().unwrap()).collect();
   //println!("SPLIT: {:?}", elf_one);
-  if elf_one[0] < elf_two[0] {
+  if elf_one[0] == elf_two[0] || elf_one[1] == elf_two[1] {
+    return true;
+  } else if elf_one[0] < elf_two[0] {
     if elf_one[1] >= elf_two[1] {
       return true;
     } else {
@@ -30,8 +32,6 @@ fn is_contain_section(sections_info: &String) -> bool {
     } else {
       return false;
     }
-  } else if elf_one[0] == elf_two[0] {
-    return true;
   } else {
     println!("HERE");
     return false;
